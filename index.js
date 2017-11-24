122aeae1f5e28c8c3198678e763848d9247b6922
Lsøng <song940@gmail.com>
2017-11-24 18:41:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function HomeKit(){
 
 };
 
-HomeKit.uuid           = require('./lib/util/uuid');
+HomeKit.UUID           = require('./lib/util/uuid');
 HomeKit.Bridge         = require('./lib/Bridge');
 HomeKit.Accessory      = require('./lib/Accessory');
 HomeKit.Service        = require('./lib/Service');
